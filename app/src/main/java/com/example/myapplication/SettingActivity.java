3794837abe18b678bcfd84af08106c36a7d8aa7e
Cheezeblokz <jingxi2001@gmail.com>
2022-05-19 15:42:38 +0800

    layout changes: to single page with toolbar and bottom navigation bar

---
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -0,0 +1,15 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class SettingActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings);
+    }
+}
