e8ff238dc97873f2d0a96f242a1de807173a4eb4
Cheezeblokz <jingxi2001@gmail.com>
2022-05-23 21:41:35 +0800

    layout change: root layout type to relativeLayout, preparing to change main area into fragment for easier navigation

--- app/src/main/java/com/example/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -11,5 +11,7 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+        Toolbar myToolbar = findViewById(R.id.settingsToolbar);
+        setSupportActionBar(myToolbar);
     }
 }
