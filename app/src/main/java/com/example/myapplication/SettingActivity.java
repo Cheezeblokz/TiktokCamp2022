42da3820a7d9c0156352749b539f2b90b68af39a
Cheezeblokz <jingxi2001@gmail.com>
2022-05-26 20:25:38 +0800

    layout changes: - added back button for settings page - added notification and night theme switches in settings page (notification switch doesn't work yet)

--- app/src/main/java/com/example/myapplication/SettingActivity.java
+++ app/src/main/java/com/example/myapplication/SettingActivity.java
@@ -1,17 +1,56 @@
 package com.example.myapplication;
 
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.appcompat.widget.Toolbar;
+
+import com.google.android.material.switchmaterial.SwitchMaterial;
 
 public class SettingActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings);
+
+        //Set toolbar for settings page
         Toolbar myToolbar = findViewById(R.id.settingsToolbar);
         setSupportActionBar(myToolbar);
+
+        //Configure back button for toolbar
+        myToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        //Show back button for toolbar
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        //Check for the theme and set the switch for theme
+        //Configure night mode switch
+        SwitchMaterial theme = findViewById(R.id.themeSwitch);
+        switch (AppCompatDelegate.getDefaultNightMode()){
+            case AppCompatDelegate.MODE_NIGHT_NO:
+                theme.setChecked(false);
+                break;
+            case AppCompatDelegate.MODE_NIGHT_YES:
+                theme.setChecked(true);
+        }
+        theme.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+            }else{
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+            }
+        });
+
+        //Configure notification switch
+        SwitchMaterial notification = findViewById(R.id.notificationSwitch);
     }
 }
