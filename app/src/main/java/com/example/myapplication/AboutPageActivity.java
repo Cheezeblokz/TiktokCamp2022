29ddee1f94c10d0a2be485771df8d38b6a0839df
JH Fan <fan.jh@outlook.com>
2022-05-26 16:06:07 +0800

    develop AboutPage feature import from: https://github.com/medyo/android-about-page

--- app/src/main/java/com/example/myapplication/AboutPageActivity.java
+++ app/src/main/java/com/example/myapplication/AboutPageActivity.java
@@ -3,6 +3,10 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import mehdi.sakout.aboutpage.AboutPage;
+import mehdi.sakout.aboutpage.Element;
 
 public class AboutPageActivity extends AppCompatActivity {
 
@@ -10,5 +14,34 @@ public class AboutPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_page);
+
+        Element project_detail = new Element();
+        project_detail.setTitle("This is the project detail. TBD...");
+
+        View about_page = new AboutPage(this)
+                //.isRTL(false)
+                //.setCustomFont(String) // or Typeface
+                .setDescription("An Air Quality Index APP\nV1.0 May 2022\n\nGroup Project @TikTokYouthCamp2022")
+
+                .addGroup("About this project")
+                .addItem(project_detail)
+                .addItem(project_detail)
+                .addItem(project_detail)
+
+                .addGitHub("https://github.com/Cheezeblokz/TiktokCamp2022")
+                .addGroup("")
+
+                .addGroup("Our group Members")
+                // members' names and emails
+                .addEmail("123@123.com", "Jingxi")
+                .addEmail("234@234.com", "Hongyi")
+                .addEmail("123@123.com", "Yee Sen")
+                .addEmail("234@234.com", "Hong Yuan")
+                .addEmail("123@123.com", "Jiahan")
+                .addGroup("")
+
+                .create();
+        setContentView(about_page);
+
     }
 }
